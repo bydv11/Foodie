bfb6a463fe12272374d851323c86ee739a45cc6c
nehaaydv11 <103181009+nehaaydv11@users.noreply.github.com>
2024-06-03 20:59:34 +0545

    Online Food Ordering Website

---
+++ admin/partials/footer.php
@@ -0,0 +1,12 @@
+<!-- Footer Section Starts -->
+<div class="footer">
+            <div class="wrapper">
+            <div class="container text-center">
+            <p>© Copyright 2023 Foodie</p>
+        </div>       
+        </div>
+        </div>
+        <!-- Footer Section Ends -->
+
+    </body>
+</html>
